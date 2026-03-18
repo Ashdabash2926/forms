d00af3e5d54d05f2aa3eceebe7a21cce3e9ef6c8
Ash <lewisashleybutterfield@googlemail.com>
2026-03-18 14:21:13 -0400

    Fix PDFKit font files missing in Vercel serverless deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['pdfkit'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/submit': ['./node_modules/pdfkit/js/data/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
